21c8c59538084e54db6853a209da26e66e694f83
Rodrigo Araujo <rodrigo.silva1987@outlook.com>
2020-12-19 20:44:03 -0300

    First controller created

--- client/app/domain/negociacao/Negociacao.js
+++ client/app/domain/negociacao/Negociacao.js
@@ -1,7 +1,5 @@
 class Negociacao {
-
     constructor(_data, _quantidade, _valor) {
-
         Object.assign(this, {_quantidade, _valor});
         this._data = new Date(_data.getTime());
         Object.freeze(this);
