7a4ddb9d11a4c9ab0e17d8868b28e4104f074d2f
Rodrigo Araujo <rodrigo.silva1987@outlook.com>
2020-12-21 21:07:02 -0300

    Negociacoes controller created

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -3,26 +3,32 @@ class NegociacaoController {
     constructor() {
 
         let $ = document.querySelector.bind(document);
-        
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+        this._negociacoes = new Negociacoes();
     }
 
     adiciona(event) {
 
         event.preventDefault();
+        this._negociacoes.adiciona(this._criaNegociacao());
+        this._limpaFormulario();
 
-        let negociacao = new Negociacao(
+    }
+
+    _limpaFormulario() {
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1; 
+        this._inputValor.value = 0.0;
+        this._inputData.focus();
+    }
+
+    _criaNegociacao() {
+        return new Negociacao(
             DateConverter.paraData(this._inputData.value),
             parseInt(this._inputQuantidade.value),
             parseFloat(this._inputValor.value)
         );
-        
-        console.log(negociacao.data)
-
-        let diaMesAno = DateConverter.paraTexto(negociacao.data);
-
-        console.log(diaMesAno)
-    }
-}
+    };
+};
